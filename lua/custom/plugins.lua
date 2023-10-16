c68c922b986ccb7d89b25695824fe75a9ca240d9
zhangzheng <zhangxiaode0@gmail.com>
2023-10-16 16:59:21 +0800

    switch to hop

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -114,10 +114,32 @@ local plugins = {
     lazy = false,
   },
   {
-    'ggandor/leap.nvim',
+   "phaazon/hop.nvim",
+    branch = "v2",
     lazy = false,
     config = function()
-      require("leap").add_default_mappings()
+      local hop = require('hop')
+      local directions = require('hop.hint').HintDirection
+      vim.keymap.set('', 't', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end, {remap=true})
+      vim.keymap.set('', 'T', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end, {remap=true})
+
+      vim.keymap.set('', '<leader>w', function()
+        hop.hint_words()
+      end, {remap=true})
+
+      vim.keymap.set('', 's', function()
+        hop.hint_char1()
+      end, {remap=true})
+
+      vim.keymap.set('', 'S', function()
+        hop.hint_char1({multi_windows=true})
+      end, {remap=true})
+
+      hop.setup()
     end,
   },
   {
